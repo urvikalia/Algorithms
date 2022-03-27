a4e39d48040f41defe3c7533b5cbba71066ae10a
urvikalia <in-urvi.kalia@urvikalias-MacBook-Pro.local>
2022-03-27 20:44:22 +0530

    first commit

---
+++ src/com/tryharder/Main.java
@@ -0,0 +1,11 @@
+package com.tryharder;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("programming in java after 2 years");
+        System.out.println("Hope to become an expert in data structures");
+    }
+}
