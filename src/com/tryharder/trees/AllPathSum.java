e8505e251b1898707164dab0aadbd52cc7a52188
urvi <urvikalia@gmail.com>
2022-05-05 10:47:25 +0530

    Trees

---
+++ src/com/tryharder/trees/AllPathSum.java
@@ -0,0 +1,38 @@
+package com.tryharder.trees;
+
+import java.util.ArrayList;
+
+public class AllPathSum {
+
+
+static class BinaryTreeNode {
+     Integer value;
+     BinaryTreeNode left;
+     BinaryTreeNode right;
+
+     BinaryTreeNode(Integer value) {
+         this.value = value;
+         this.left = null;
+         this.right = null;
+     }
+ }
+
+    /* Given a binary tree and an integer k, find all the root to leaf paths that sum to k. */
+
+
+    static void helper(BinaryTreeNode root, Integer k,ArrayList<Integer> slate ,ArrayList<ArrayList<Integer>> result)
+    {
+        
+
+    }
+    static ArrayList<ArrayList<Integer>> all_paths_sum_k(BinaryTreeNode root, Integer k) {
+        // Write your code here.
+        return new ArrayList();
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/tryharder/trees/DiameterOfTree.java
@@ -0,0 +1,22 @@
+package com.tryharder.trees;
+
+public class DiameterOfTree {
+
+/*    Notes
+    Diameter of a binary tree is the length of the longest path between any two nodes of the tree.
+    Length between any two nodes is equal to the number of edges traversed to reach one node from the other.
+    Constraints:
+
+            1 <= number of nodes in the given tree <= 105
+            0 <= node value < number of nodes
+    Node values are unique*/
+
+    static Integer binary_tree_diameter(BinaryTreeNode root) {
+        // Write your code here.
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
